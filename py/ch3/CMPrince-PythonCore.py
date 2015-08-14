120a8e41f7ebb13ac41b2bfa66ecc0b3c2e36016
cmprince <chrprince@gmail.com>
2015-08-14 00:56:49 -0400

    tweak tweak

--- py/ch3/CMPrince-PythonCore.py
+++ py/ch3/CMPrince-PythonCore.py
@@ -8,6 +8,7 @@ import csv                  #for reading the citi bike data file from csv
 import numpy as np          #for numpy arrays on which to do the analysis
 import datetime as dt       #for string to time formatting
 import matplotlib as mpl
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib import pyplot as pl
 
 def getCBData(fname, cols=[], dtypes = [np.object0]):
@@ -135,42 +136,69 @@ pctFemale = totalFemale/numTotal
 
 figh = pl.figure()
 ax1 = pl.subplot2grid((3,6), (0,0), colspan=6)
-ax2 = pl.subplot2grid((3,6), (1,0), colspan=2)
-ax3 = pl.subplot2grid((3,6), (2,0), colspan=2, sharex=ax2, sharey=ax2)
-ax4 = pl.subplot2grid((3,6), (1,2), rowspan=2)
-ax5 = pl.subplot2grid((3,6), (1,3), rowspan=2)
-ax6 = pl.subplot2grid((3,6), (1,4), colspan=2, sharex=ax2, sharey=ax2)
-ax7 = pl.subplot2grid((3,6), (2,4), colspan=2, sharex=ax2, sharey=ax2)
+ax2 = pl.subplot2grid((3,6), (1,1), colspan=2)
+ax3 = pl.subplot2grid((3,6), (2,1), colspan=2, sharex=ax2, sharey=ax2)
+ax4 = pl.subplot2grid((3,6), (1,0), rowspan=2)
+ax5 = pl.subplot2grid((3,6), (1,5), rowspan=2)
+ax6 = pl.subplot2grid((3,6), (1,3), colspan=2, sharex=ax2, sharey=ax2)
+ax7 = pl.subplot2grid((3,6), (2,3), colspan=2, sharex=ax2, sharey=ax2)
 
 pl.sca(ax1)
 ax1.hist(hours, 24, range=(0,24), align='left', color='gray', label='Total rides')
-l1, = pl.plot(numMale, label='Male')
-l2, = pl.plot(numFemale, label='Female')
-l3, = pl.plot(numUnknown, label='Unknown gender')
-ax1.legend(loc='upper left')
+l1, = pl.plot(numMale, label='Male', color='b', linewidth=3)
+l2, = pl.plot(numFemale, label='Female', color='r', linewidth=3)
+l3, = pl.plot(numUnknown, label='Unknown', color='g', linewidth=3)
+l4, = pl.plot(numCust, label='Customers', color='yellow', linewidth=3)
+l5, = pl.plot(numSubsc, label='Subscribers', color='pink', linewidth=3)
+ax1.legend(loc='upper left', ncol=2)
 ax1.set_xlim(left=-1,right=24)
+ax1.set_xticks([0,6,12,18,23])
+pl.title('Total rides in period by hour, per gender and usertype')
+pl.xlabel('hour of the day', labelpad=0.05)
+curpos=ax1.get_position()
+ax1.set_position([.125,.725,.775,.225]) #[curpos.bounds[0], 0.8, curpos.bounds[2], curpos.bounds[3]])
 
 pl.sca(ax2)
-c,x,y,mh2 = ax2.hist2d(menhours, mendays, [range(0,25),range(1,33)])
+c2,x,y,mh2 = ax2.hist2d(menhours, mendays, [range(0,25),range(1,33)])
 ax2.set_xlim(right=24)
-pl.colorbar(mh2)
+pl.setp( ax2.get_yticklabels(), visible=False)
+pl.xlabel('hour of the day', labelpad=0.03)
+divider2 = make_axes_locatable(ax2)
+cax2=divider2.append_axes("top", size='5%', pad=0.03)
+cbar1=figh.colorbar(mh2, cax=cax2, orientation = 'horizontal')
+cbar1.ax.tick_params(labeltop='on', labelbottom='off', top='on', bottom='off')
+ax2.text(0,0.5,'male riders', color='white', rotation='vertical', verticalalignment='bottom')
+ax2.set_xticks([0,6,12,18,23])
+ax2.set_yticks([7,14,21,28])
  
 pl.sca(ax3)
 c,x,y,wh2 = ax3.hist2d(womenhours, womendays, [range(0,25),range(1,33)])
 pl.setp( ax3.get_xticklabels(), visible=False)
-pl.colorbar(wh2)
+pl.setp( ax3.get_yticklabels(), visible=False)
+divider3 = make_axes_locatable(ax3)
+cax3=divider3.append_axes("bottom", size='5%', pad=0.03)
+cbar2=figh.colorbar(wh2, cax=cax3, orientation = 'horizontal')
+ax3.text(0,0.5,'female riders', color='white', rotation='vertical', verticalalignment='bottom')
 
 pl.sca(ax6)
-c,x,y,ch2 = ax6.hist2d(custhours, custdays, [range(0,25),range(1,33)])
+c6,x,y,ch2 = ax6.hist2d(custhours, custdays, [range(0,25),range(1,33)])
 ax6.set_xlim(right=24)
-pl.colorbar(ch2)
- 
+pl.ylabel('day of the month', labelpad=0.03)
+divider6 = make_axes_locatable(ax6)
+cax6=divider6.append_axes("top", size='5%', pad=0.03)
+cbar6=figh.colorbar(ch2, cax=cax6, orientation = 'horizontal')
+cbar6.ax.tick_params(labeltop='on', labelbottom='off', top='on', bottom='off')
+ax6.text(0,0.5,'customers', color='white', rotation='vertical', verticalalignment='bottom')
+
 pl.sca(ax7)
 c,x,y,sh2 = ax7.hist2d(subschours, subscdays, [range(0,25),range(1,33)])
 pl.setp( ax7.get_xticklabels(), visible=False)
-pl.colorbar(sh2)
+divider7 = make_axes_locatable(ax7)
+cax7=divider7.append_axes("bottom", size='5%', pad=0.03)
+figh.colorbar(sh2, cax=cax7, orientation = 'horizontal')
+ax7.text(0,0.5,'subscribers', color='white', rotation='vertical', verticalalignment='bottom')
 
-collist1 = ['r', 'g', 'b']
+collist1 = ['b', 'r', 'g']
 lablist1 = ['Men', 'Women', 'Unknown']
 bounds1 = [0, totalMale, totalMale + totalFemale, numTotal]
 cb2 = myLabeledColorBar(ax4, bounds1, lablist1, collist1, orientation='vertical')
