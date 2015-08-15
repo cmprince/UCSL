930c4d3993de99b67c331aa3703cccad5175f40f
cmprince <chrprince@gmail.com>
2015-08-15 10:57:12 -0400

    Final version to submit.

--- py/ch3/CMPrince-PythonCore.py
+++ py/ch3/CMPrince-PythonCore.py
@@ -1,25 +1,23 @@
 '''
+Solution to UCSL Python core challenge
 Created on Aug 8, 2015
 
-@author: cmp670
+@author: cmp670@nyu.edu
 '''
 
-import csv                  #for reading the citi bike data file from csv
-import numpy as np          #for numpy arrays on which to do the analysis
-import datetime as dt       #for string to time formatting
-import matplotlib as mpl
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from matplotlib import pyplot as pl
-import pandas as pd
+import numpy as np                      #for numpy arrays on which to do the analysis
+import datetime as dt                   #for string to time formatting
+import matplotlib as mpl                #the graphing utility
+from matplotlib import pyplot as pl     #shortcut for the main plotting functions
+from mpl_toolkits.axes_grid1 import make_axes_locatable     #see addCBar function for use
+import pandas as pd                     #for reading the citi bike data from csv into dataframe
 
 def myLabeledColorBar(axes, bounds, textlabs=[''], colorlist=['gray'], drawedges = True, orientation = 'horizontal'):
+    #use color list to create colormap for colorbar
     cmap = mpl.colors.ListedColormap(colorlist)
-
-    # If a ListedColormap is used, the length of the bounds array must be
-    # one greater than the length of the color list.  The bounds must be
-    # monotonically increasing.
-
     norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+    
+    #create the colorbar with the boundaries passed in bounds
     cb = mpl.colorbar.ColorbarBase(axes, cmap=cmap,
                                          norm=norm,
                                          boundaries=bounds,
@@ -27,10 +25,12 @@ def myLabeledColorBar(axes, bounds, textlabs=[''], colorlist=['gray'], drawedges
                                          spacing='proportional',
                                          orientation=orientation)
 
+    #Draw labels along the x-axis if the colorbar is horizontal, otherwise draw them on the y-axis.
+    #Draw the labels in the centers of the regions set by the boundaries.
     if (orientation == 'horizontal'):
         cb.ax.get_xaxis().set_ticks([])
         for j in range(len(bounds[1:])):
-            cb.ax.text(((bounds[j+1]-bounds[j])/2 + bounds[j])/bounds[-1],
+            cb.ax.text(((bounds[j+1]-bounds[j])/2 + bounds[j])/float(bounds[-1]),
                        .5,
                        textlabs[j % len(textlabs)] + '\n' + '{0:g}'.format((bounds[j+1]-bounds[j])),
                        ha='center', va='center')
@@ -40,7 +40,7 @@ def myLabeledColorBar(axes, bounds, textlabs=[''], colorlist=['gray'], drawedges
         cb.ax.get_yaxis().set_ticks([])
         for j in range(len(bounds[1:])):
             cb.ax.text(.5,
-                       ((bounds[j+1]-bounds[j])/2 + bounds[j])/bounds[-1],
+                       ((bounds[j+1]-bounds[j])/2 + bounds[j])/float(bounds[-1]),
                        textlabs[j % len(textlabs)] + '\n' + '{0:g}'.format((bounds[j+1]-bounds[j])),
                        ha='center', va='center')
                        #bbox=dict(facecolor='white', ec='none', alpha=0.65))
@@ -48,6 +48,7 @@ def myLabeledColorBar(axes, bounds, textlabs=[''], colorlist=['gray'], drawedges
     return cb
 
 def addCBar(im, ax, ticks=5, side='top', size='5%', pad=0.03):
+    #create region in ax in which to draw colorbar
     divider = make_axes_locatable(ax)
     caxnew=divider.append_axes(side, size=size, pad=pad)
     
@@ -55,7 +56,7 @@ def addCBar(im, ax, ticks=5, side='top', size='5%', pad=0.03):
     orientation='horizontal'
     if (side=='left' or side=='right'): orientation='vertical'
     
-    #Create the colorbar and set tick locations from the side argument
+    #Create the colorbar from data im and set tick locations from the side argument
     cbar=figh.colorbar(im, cax=caxnew, orientation = orientation)
     cbar.ax.tick_params(labeltop=(side=='top'), labelbottom=(side=='bottom'),
                         top=(side=='top'), bottom=(side=='bottom'),
@@ -72,9 +73,10 @@ def addCBar(im, ax, ticks=5, side='top', size='5%', pad=0.03):
 ########
 #Read file into a pandas data frame
 fname = '../../R/ch2/2014-07 - Citi Bike trip data.csv'
-cbdata=pd.read_csv(fname, parse_dates=['starttime'],nrows=10000)
+dataperiod = 'July 2014'        #for graph titling (so change this for a different data set)
+cbdata=pd.read_csv(fname, parse_dates=['starttime']) #<- nrows for testing purposes
 
-#Sum for each gender type, get the total, and calculate percentages
+#Subset start days and times by genders and usertypes 
 hours = np.zeros(len(cbdata))
 days = np.zeros(len(cbdata))
 hours = [cbdata['starttime'][h].hour for h in range(len(cbdata))]
@@ -83,41 +85,32 @@ menhours = [cbdata['starttime'][h].hour for h in range(len(cbdata)) if cbdata['g
 mendays = [cbdata['starttime'][d].day for d in range(len(cbdata)) if cbdata['gender'][d] == 1]
 womenhours = [cbdata['starttime'][h].hour for h in range(len(cbdata)) if cbdata['gender'][h] == 2] 
 womendays = [cbdata['starttime'][d].day for d in range(len(cbdata)) if cbdata['gender'][d] == 2]
+unknownhours = [cbdata['starttime'][h].hour for h in range(len(cbdata)) if cbdata['gender'][h] == 0]
 custhours = [cbdata['starttime'][h].hour for h in range(len(cbdata)) if cbdata['usertype'][h] == 'Customer'] 
 custdays = [cbdata['starttime'][d].day for d in range(len(cbdata)) if cbdata['usertype'][d] == 'Customer']
 subschours = [cbdata['starttime'][h].hour for h in range(len(cbdata)) if cbdata['usertype'][h] == 'Subscriber'] 
 subscdays = [cbdata['starttime'][d].day for d in range(len(cbdata)) if cbdata['usertype'][d] == 'Subscriber']
 
-print cbdata['starttime'][1].hour
-
-numMale, numFemale, numUnknown, numCust, numSubsc = np.zeros(24),np.zeros(24),np.zeros(24),np.zeros(24),np.zeros(24)
-for i in range(len(cbdata)):
-    sh = cbdata['starttime'][i].hour
-    if cbdata['gender'][i]==1:
-        numMale[sh] += 1
-    elif cbdata['gender'][i] == 2:
-        numFemale[sh] += 1
-    elif cbdata['gender'][i] == 0:
-        numUnknown[sh] += 1
-    
-    if cbdata['usertype'][i] == 'Customer':
-        numCust[sh] += 1
-    elif cbdata['usertype'][i] == 'Subscriber':
-        numSubsc[sh] += 1
+#Compute histograms for each gender and usertype
+numMale, binsm = np.histogram(menhours,range(0,24))
+numFemale, binsf = np.histogram(womenhours,range(0,24))
+numUnknown, binsu = np.histogram(unknownhours,range(0,24))
+numCust, binsc = np.histogram(custhours,range(0,24))
+numSubsc, binss = np.histogram(subschours,range(0,24))
            
-print (cbdata['usertype'])
-print (numMale, numFemale, numUnknown, numCust, numSubsc)
-
 totalMale = numMale.sum()
 totalFemale = numFemale.sum()
 totalUnknown = numUnknown.sum()
 totalCust = numCust.sum()
 totalSubsc = numSubsc.sum()
-numTotal = totalMale + totalFemale + totalUnknown
+numTotal = totalMale + totalFemale + totalUnknown   #should be equal to len(cbdata)
 
-pctMale = totalMale/numTotal
-pctFemale = totalFemale/numTotal
+#Caluclate the average tripduration (first part of challenge):
+avTripduration = cbdata['tripduration'].mean()
+print 'Read n=' + str(len(cbdata)) + ' records from data set ' + fname + \
+      '. The average tripduration is ' + '{0:.2f}'.format(avTripduration) + ' seconds.'
 
+#Visualization (second part of challenge):
 #Setup figure subplot grid:
 #=============
 #|11111111111|
@@ -135,18 +128,20 @@ ax4 = pl.subplot2grid((3,6), (1,3), colspan=2, sharex=ax2, sharey=ax2)
 ax5 = pl.subplot2grid((3,6), (2,3), colspan=2, sharex=ax2, sharey=ax2)
 ax6 = pl.subplot2grid((3,6), (1,0), rowspan=2)
 ax7 = pl.subplot2grid((3,6), (1,5), rowspan=2)
+#It's a crowded plot, so let's use most of the canvas:
 figh.subplots_adjust(top=0.95,bottom=0.05,left=0.1,right=0.95)
 
+#Construct a histogram for the rides by hour
 pl.sca(ax1)
 bincolors=['lightgray']*4 + ['khaki']*4
 c,b,patches=ax1.hist(hours, 24,
                      range=(0,24), align='left',
                      color='gray', label='Total rides')
-#Color bins using bincolors ('%' permits recycling of the list)
+#Color bins using bincolors ('% len(list)' permits recycling of the list)
 for i,patch in enumerate(patches):
     patch.set_facecolor(bincolors[i % len(bincolors)])  
     patch.set_edgecolor('gray')
-#Add lines to the plot for each studied group
+#Add lines to the plot for each group histogram
 l1, = pl.plot(numMale, label='Male', color='deepskyblue', linewidth=3)
 l2, = pl.plot(numFemale, label='Female', color='tomato', linewidth=3)
 l3, = pl.plot(numUnknown, label='Unknown', color='limegreen', linewidth=3)
@@ -158,7 +153,7 @@ ax1.legend(loc='upper left', ncol=1, fontsize='small')
 #so adjust the range to extend a little beyond -0.5 and +23.5
 ax1.set_xlim(left=-1,right=24)
 ax1.set_xticks([0,4,8,12,16,20,23])
-pl.title('Total rides in period by hour, per gender and usertype')
+pl.title('Total Citi Bike rides in ' + dataperiod + ' by hour, per gender and usertype')
 pl.xlabel('hour of the day', labelpad=0.1)
 #manually bump the axis up the figure a bit to avoid label interference
 ax1.set_position([.1,.725,.85,.225])
@@ -187,7 +182,8 @@ pl.sca(ax3)
 c,x,y,wh2 = ax3.hist2d(womenhours, womendays, [range(0,25),range(1,33)])
 pl.ylabel('day of the month', labelpad=0)
 cbar3=addCBar(wh2,ax3, side='bottom')
-ax3.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False, left=False, labelleft=False, labelsize='small', pad=0)
+ax3.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False, 
+                left=False, labelleft=False, labelsize='small', pad=0)
 ax3.text(0,1,'women',
          color='white', horizontalalignment='left', verticalalignment='top',
          transform=ax3.transAxes)
